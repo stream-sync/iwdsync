9382adbc674d669dc394f77d55e7f0ec5593e964
IWDominate305 <68765395+IWDominate305@users.noreply.github.com>
2020-07-25 08:05:57 -0700

    Update gen.js

--- src/configs/gen.js
+++ src/configs/gen.js
@@ -3,7 +3,7 @@ export const caster_data = {
         config_url: 'https://hastebin.com/raw/ucubenoqay',
         preset: {
             title: 'iwilldominate',
-            youtube_live_url: 'https://www.youtube.com/watch?v=CJ57y4ZzPzU',
+            youtube_live_url: 'https://www.youtube.com/watch?v=d2Hc7eChnio',
             twitch_channel: 'iwilldominate',
             lcs: {
                 channel: 'lcs'
