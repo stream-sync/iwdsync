9dede3ec23ab259886d92507e2158dcc1cf58db7
Brian Perrett <perrettbrian@gmail.com>
2020-07-26 02:44:19 -0600

    shut off youtube for now

--- src/configs/gen.js
+++ src/configs/gen.js
@@ -2,7 +2,7 @@ export const caster_data = {
     iwd: {
         preset: {
             title: 'iwilldominate',
-            youtube_live_url: 'https://www.youtube.com/watch?v=v8xf49K1rO0',
+            youtube_live_url: '',
             twitch_channel: 'iwilldominate',
         }
     }
