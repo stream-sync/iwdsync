62ee66a823227973332cd9c5d8014a3b75bbcffd
Brian Perrett <perrettbrian@gmail.com>
2020-07-27 11:12:26 -0600

    use controlled youtube embed rather than plain iframe

--- src/components/caster/youtubeembed.jsx
+++ src/components/caster/youtubeembed.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 export function YoutubeEmbed(props) {
     let youtube_live_url = props.youtube_live_url
     const width = props.width || 640
+    let youtube_id = null
     if (youtube_live_url) {
-        // let id = youtube_live_url.split('?v=')[1]
-        youtube_live_url =
-            youtube_live_url.replace('/watch?v=', '/embed/') + '?autoplay=1'
+        youtube_id = youtube_live_url.split('?v=')[1]
+        youtube_live_url = youtube_live_url.replace('/watch?v=', '/embed/') + '?autoplay=1'
     }
 
     return (
         <div>
             {youtube_live_url && (
-                <YoutubeIframe width={width} url={youtube_live_url} />
+                <YoutubeIframe youtube_id={youtube_id} width={width} url={youtube_live_url} />
             )}
         </div>
     )
 }
 
 function YoutubeIframe(props) {
+    const [player, setPlayer] = useState(null)
+
+    const createPlayer = useCallback(() => {
+        let new_player = new window.YT.Player('ytplayer', {
+            videoId: props.youtube_id,
+        })
+        setPlayer(new_player)
+    }, [props.youtube_id])
+
+    // set player on load
+    useEffect(() => {
+        window.YT.ready(createPlayer)
+    }, [createPlayer])
+
     return (
-        <div
-            style={{maxWidth: props.width, width: props.width}}
-            className="video-wrap">
+        <div style={{ maxWidth: props.width, width: props.width }} className="">
             <div className="video-container">
-                <iframe
-                    title="youtube-embed"
-                    src={props.url}
-                    frameBorder="0"
-                    autoplay='on'
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
+                <div id="ytplayer"></div>
+                {/* <iframe */}
+                {/*     title="youtube-embed" */}
+                {/*     src={props.url} */}
+                {/*     frameBorder="0" */}
+                {/*     autoplay='on' */}
+                {/*     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" */}
+                {/*     allowFullScreen */}
+                {/* ></iframe> */}
             </div>
         </div>
     )
