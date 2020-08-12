be5fd76b672b77e482b9615b9d6ed807e48f899b
Raphael Beer <raphael.beer@protonmail.com>
2020-08-12 17:06:54 +0200

    Remove: empty <div>s

--- src/App.js
+++ src/App.js
@@ -6,25 +6,18 @@ import {
     Redirect,
     // Link
 } from 'react-router-dom'
+
 import { Caster } from './components/caster/caster'
-// import { Home } from './components/general/Home'
 import './App.css'
 
 function App() {
     return (
-        <div>
-            <Router>
-                <div>
-                    <Switch>
-
-                        <Route path="/" exact component={TempReroute} />
-                        <Route path="/caster/:caster" component={Caster} />
-                        {/* <Route path="/config" component={Caster} /> */}
-                    </Switch>
-
-                </div>
-            </Router>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={TempReroute} />
+                <Route path="/caster/:caster" component={Caster} />
+            </Switch>
+        </Router>
     )
 }
 
