d952cb492a410d1f99636ec5a33aedb7ff397f51
Raphael Beer <raphael.beer@protonmail.com>
2020-08-15 21:23:49 +0200

    Add: read API domain from env - REACT_APP_DOMAIN

--- src/api/domain.js
+++ src/api/domain.js
@@ -1,8 +1,3 @@
-let domain = 'https://iwdsync.herokuapp.com'
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    domain = 'http://localhost:8000'
-}
-else {
-    domain = 'https://iwdsync.herokuapp.com'
-}
-export default domain
+export default process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000'
+    : process.env.REACT_APP_DOMAIN || 'https://iwdsync.herokuapp.com'
