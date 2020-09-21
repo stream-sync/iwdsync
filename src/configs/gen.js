9d4f4bfefe3818e3398a4f48e7921abb20045482
Brian Perrett <perrettbrian@gmail.com>
2020-09-21 01:05:10 -0600

    fix parents

--- src/configs/gen.js
+++ src/configs/gen.js
@@ -10,11 +10,12 @@ export const caster_data = {
 
 export const parents = [
     'iwdlive.com',
-    'iwdsync.app',
-    'iwdsync.vercel.app',
+    'costream.me',
+    // 'iwdsync.app',
+    // 'iwdsync.vercel.app',
     // 'iwdsync-git-master.antigravity.vercel.app',
     // 'iwdsync.antigravity.vercel.app',
-    'iwdsync.import-antigravity.vercel.app',
-    'iwdsync-git-master.import-antigravity.vercel.app',
+    // 'iwdsync.import-antigravity.vercel.app',
+    // 'iwdsync-git-master.import-antigravity.vercel.app',
     'localhost',
 ]
